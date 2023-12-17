df4811af4f4f8b0b94398eea4caf51bbdd411626
tomansvk <tomanka.filip@gmail.com>
2023-12-17 16:40:52 +0100

    added statistics parameters

--- employeeAnalyzer.js
+++ employeeAnalyzer.js
@@ -1,18 +1,109 @@
-function getEmployeeStatistics(dtoOut) {
+function calculateAge(birthdate, currentDate) {
+    const birthDateObj = new Date(birthdate);
+    let age = currentDate.getFullYear() - birthDateObj.getFullYear();
+  
+    // Check if the birthdate has occurred this year
+    const currentMonth = currentDate.getMonth();
+    const birthMonth = birthDateObj.getMonth();
+    const currentDay = currentDate.getDate();
+    const birthDay = birthDateObj.getDate();
+  
+    if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
+      age--;
+    }
+  
+    return age;
+}
 
-    const statistics = [];
+function addToOrderedArray(orderedArray, newValue) {
 
-    const totalWorkload = dtoOut.reduce((sum, employee) => sum + employee.workload, 0);
-    const averageWorkload = totalWorkload / dtoOut.length;
-    
-    const employeesCount = dtoOut.length;
+    if (orderedArray.length === 0 || newValue >= orderedArray[orderedArray.length - 1]) {
+        orderedArray.push(newValue);
+    } else {
+        let indexToInsert = orderedArray.length - 1;
+        while (indexToInsert >= 0 && newValue < orderedArray[indexToInsert]) {
+          indexToInsert--;
+        }
+        orderedArray.splice(indexToInsert + 1, 0, newValue);
+    }
 
-    const statisticsData = {
-        averageWorkload: averageWorkload,
-        employeesCount: employeesCount
+}
+
+function getMedian(orderedArray) {
+
+    const isEven = orderedArray.length % 2 === 0;
+    const middleOfArray = orderedArray.length/2;
+
+    if (isEven) {
+        return ((orderedArray[(middleOfArray-1)] + orderedArray[middleOfArray])/2);
+    } else {
+        return (orderedArray[Math.floor(middleOfArray)]);
+    }
+}
+
+function getEmployeeStatistics(employees) {
+
+    // define output structure
+    const statistics = {
+        total:0,
+        workload10:0,
+        workload20:0,
+        workload30:0,
+        workload40:0,
+        averageAge:0,
+        minAge:0,
+        maxAge:0,
+        medianAge:0,
+        medianWorkload:0,
+        averageWomenWorkload:0
     };
 
-    statistics.push(statisticsData)
+    // define other variables
+    let womenWorkload = 0;
+    let womenCount = 0; 
+    const orderedWorkloads = [];
+    const currentDate = new Date();
+    const orderedAges = [];
+
+    // loop through employees data and gather info
+    for (let i = 0; i < employees.length; i++) {
+        const employee = employees[i];
+        const workload = employee.workload;
+
+        switch (workload) {
+            case 10:
+              statistics.workload10++;
+              break;
+            case 20:
+              statistics.workload20++;
+              break;
+            case 30:
+              statistics.workload30++;
+              break;
+            case 40:
+              statistics.workload40++;
+              break;
+        }
+
+        addToOrderedArray(orderedWorkloads, workload);
+
+        if (employee.gender === 'female') {
+            womenWorkload += workload;
+            womenCount++;
+        }
+
+        const age = calculateAge(employee.birthdate, currentDate);
+        addToOrderedArray(orderedAges, age);
+
+    }
+
+    statistics.total = employees.length;
+    statistics.averageWomenWorkload = womenWorkload / womenCount;
+    
+    statistics.medianWorkload = getMedian(orderedWorkloads);
+
+    console.log(orderedWorkloads);
+
     return statistics;
 
 };
